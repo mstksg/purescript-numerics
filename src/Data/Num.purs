26259b530681f999e9734b10f70803eb9f7a14ac
Michael Jones <proclivis@gmail.com>
2017-07-09 23:58:56 +0000

    Cleanup

--- src/Data/Num.purs
+++ src/Data/Num.purs
@@ -2,8 +2,7 @@
 module Data.Num
   ( class Num
   , negate, abs, signum
---  , fromInt --, toInt
-  , fromBigInt --, toBigInt
+  , fromBigInt
   ) where
 
 import Prelude (id, ($), (<<<), class Eq, class Semiring, class Ring, class CommutativeRing)
@@ -18,32 +17,21 @@ class (Eq a, Semiring a, Ring a, CommutativeRing a) <= Num a where
     negate :: a -> a
     abs :: a -> a
     signum :: a -> a
---    fromInt :: Int -> a
---    toInt :: a -> Int
     fromBigInt :: BI.BigInt -> a
---    toBigInt :: a -> BI.BigInt
 
 -- | `Num` instance for `Int`.
 instance numInt :: Num Int where
     negate = R.negate
     abs = O.abs
     signum = O.signum
---    fromInt = id
---    toInt = id
     fromBigInt bi = fromMaybe 0 $ (I.fromNumber <<< BI.toNumber) bi
---    toBigInt i = BI.fromInt i
-
--- | Probably should not be here because it is decimal. Haskell only puts CDouble here, not Double.
    
 -- | `Num` instance for `Number`.
 instance numNumber :: Num Number where
     negate = R.negate
     abs = O.abs
     signum = O.signum
---    fromInt i = I.toNumber i
---    toInt n = fromMaybe 0 $ I.fromNumber n
     fromBigInt bi = BI.toNumber bi
---    toBigInt n = BI.fromInt (fromMaybe 0 (I.fromNumber n))
 
 -- | `Num` instance for `BigInt`.
 instance numBigInt :: Num BI.BigInt where
